e450e98a8a4c8387e27785ab4839bb055855fece
hs-2171341-kimtaesoo <7947267@naver.com>
2026-09-15 17:41:28 +0900

    [Add] 적 공격 1차 제작, 죽음 상태 1차 제작

---
+++ Assets/3.Script/Enemy_Kts/EnemyBullet.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBullet : MonoBehaviour
+{
+    [SerializeField] private float speed = 20f;
+    [SerializeField] private float lifeTime = 3f;
+
+    private EnemyBulletPool enemyBulletPool;
+
+    private Vector3 direction;
+    private int damage;
+
+    private float lifeTimer;
+
+    public void Initialize(EnemyBulletPool pool)
+    {
+        enemyBulletPool = pool;
+    }
+
+    public void Fire(Vector3 direction, int damage)
+    {
+        this.direction = direction.normalized;
+        this.damage = damage;
+
+        lifeTimer = lifeTime;
+    }
+
+    private void Update()
+    {
+        transform.position += direction * speed * Time.deltaTime;
+
+        lifeTimer -= Time.deltaTime;
+
+        if (lifeTimer <= 0f)
+        {
+            ReturnToPool();
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        IDamageable target = other.GetComponent<IDamageable>();
+
+        if (target != null)
+        {
+            target.TakeDamage(damage);
+        }
+
+        ReturnToPool();
+    }
+
+    private void ReturnToPool()
+    {
+        if (enemyBulletPool != null)
+        {
+            enemyBulletPool.ReturnBullet(gameObject);
+        }
+        else
+        {
+            gameObject.SetActive(false);
+        }
+    }
+}
