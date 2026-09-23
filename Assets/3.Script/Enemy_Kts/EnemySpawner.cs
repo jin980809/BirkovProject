88010acb81563914707aa5fef2c7f35cf67fc377
hs-2171341-kimtaesoo <7947267@naver.com>
2026-09-23 16:46:17 +0900

    [add] 최적화 하고 싶다

--- Assets/3.Script/Enemy_Kts/EnemySpawner.cs
+++ Assets/3.Script/Enemy_Kts/EnemySpawner.cs
@@ -4,13 +4,21 @@ using UnityEngine;
 
 public class EnemySpawner : MonoBehaviour
 {
+    [SerializeField] private GameObject area;
+
     private void OnTriggerEnter(Collider other)
     {
-        
+        if (other.CompareTag("Player"))
+        {
+            area.SetActive(true);
+        }
     }
 
     private void OnTriggerExit(Collider other)
     {
-        
+        if (other.CompareTag("Player"))
+        {
+            area.SetActive(false);
+        }
     }
 }
